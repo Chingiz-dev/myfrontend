c725b32ad40cc051efb029c153d8e58f48b4c35d
Chingiz Shigayev <shigayevphone@gmail.com>
2021-01-19 18:10:31 +0400

    Updates

---
+++ precache-manifest.899af05cfdca106c1a378d63bc7a760d.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32241181fa14c88743891583c01f890",
+    "url": "/myfrontend/index.html"
+  },
+  {
+    "revision": "ab55567fac5b55d4cc35",
+    "url": "/myfrontend/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "390e8ad0ae8fe8e2eca4",
+    "url": "/myfrontend/static/css/main.12261d88.chunk.css"
+  },
+  {
+    "revision": "ab55567fac5b55d4cc35",
+    "url": "/myfrontend/static/js/2.552efd28.chunk.js"
+  },
+  {
+    "revision": "c692524537e36e6b8bf6c3c4cb32c075",
+    "url": "/myfrontend/static/js/2.552efd28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390e8ad0ae8fe8e2eca4",
+    "url": "/myfrontend/static/js/main.bbd58ed8.chunk.js"
+  },
+  {
+    "revision": "dfa1b69a68ed959d29f6",
+    "url": "/myfrontend/static/js/runtime-main.f47b5fca.js"
+  },
+  {
+    "revision": "1f78b41ac96b93ed89501a1032461727",
+    "url": "/myfrontend/static/media/Zara_2017 (10) 4.1f78b41a.svg"
+  },
+  {
+    "revision": "71fde4d6edc62e4af4ba483012dde6ad",
+    "url": "/myfrontend/static/media/action_icon.71fde4d6.svg"
+  },
+  {
+    "revision": "b364b59315f992875457882eab344358",
+    "url": "/myfrontend/static/media/addProduct_icon.b364b593.svg"
+  },
+  {
+    "revision": "7784772e25eab199b9f22bf75e548e39",
+    "url": "/myfrontend/static/media/arrowLeft_icon.7784772e.svg"
+  },
+  {
+    "revision": "d8e8262ae615b8bd850bbcca48a313d1",
+    "url": "/myfrontend/static/media/bi_download.d8e8262a.svg"
+  },
+  {
+    "revision": "8024192ad8b2851c7480feeee84dae54",
+    "url": "/myfrontend/static/media/box.8024192a.svg"
+  },
+  {
+    "revision": "ce66940edfca8b89d1ef36f122f00799",
+    "url": "/myfrontend/static/media/briefcase.ce66940e.svg"
+  },
+  {
+    "revision": "b81103a1877df9f55628c67f76a77b2f",
+    "url": "/myfrontend/static/media/burger_icon.b81103a1.svg"
+  },
+  {
+    "revision": "3b8bc6bbc3df6fe08b709834334ebea3",
+    "url": "/myfrontend/static/media/card_image_1.3b8bc6bb.png"
+  },
+  {
+    "revision": "d401ae30abf99be880b22cf10da5c6c2",
+    "url": "/myfrontend/static/media/card_image_2.d401ae30.png"
+  },
+  {
+    "revision": "4518b1d796ff422ff11878b13989de67",
+    "url": "/myfrontend/static/media/card_image_3.4518b1d7.png"
+  },
+  {
+    "revision": "183d9e647669bf2ed9efbcba5582c338",
+    "url": "/myfrontend/static/media/card_image_4.183d9e64.png"
+  },
+  {
+    "revision": "437a151cf9ac70757e1f5567bbc478e0",
+    "url": "/myfrontend/static/media/card_image_5.437a151c.png"
+  },
+  {
+    "revision": "84c31137b7ddde032873eddacf1f595d",
+    "url": "/myfrontend/static/media/card_image_6.84c31137.png"
+  },
+  {
+    "revision": "d07e101b7e6f21a3d92a5761e828e92f",
+    "url": "/myfrontend/static/media/close_icon.d07e101b.svg"
+  },
+  {
+    "revision": "f81760dc6c848b648c6133afea8a4fb5",
+    "url": "/myfrontend/static/media/facebook_icon.f81760dc.svg"
+  },
+  {
+    "revision": "99fd4eb0b1fd3f4ef02c8a285f2b8466",
+    "url": "/myfrontend/static/media/file-text.99fd4eb0.svg"
+  },
+  {
+    "revision": "88a4daed3f05073c948a8ea8056a9d20",
+    "url": "/myfrontend/static/media/header_banner.88a4daed.png"
+  },
+  {
+    "revision": "d16b6a71b4ed8b4ac77a53ec4b77f418",
+    "url": "/myfrontend/static/media/heart.d16b6a71.svg"
+  },
+  {
+    "revision": "c9410aa7419e7393500b107584fb3f98",
+    "url": "/myfrontend/static/media/heartPink.c9410aa7.svg"
+  },
+  {
+    "revision": "69321e4dcc6d5b8be4ab66c08c2bfee3",
+    "url": "/myfrontend/static/media/help-circle.69321e4d.svg"
+  },
+  {
+    "revision": "3598fedc494a76ca9ea6efb6186fead3",
+    "url": "/myfrontend/static/media/instagram_icon.3598fedc.svg"
+  },
+  {
+    "revision": "2baa4d8d8454fb67c062b69f9d750417",
+    "url": "/myfrontend/static/media/jsCorp_icon.2baa4d8d.svg"
+  },
+  {
+    "revision": "f529a7ad548192a4b4f4b8247be7a828",
+    "url": "/myfrontend/static/media/lk_plus_data.f529a7ad.svg"
+  },
+  {
+    "revision": "28ecb26b0f39b44f19965d91cfd4f31c",
+    "url": "/myfrontend/static/media/log-out.28ecb26b.svg"
+  },
+  {
+    "revision": "0ca8065ab0840a37ddc1d8709440b84a",
+    "url": "/myfrontend/static/media/main_logo.0ca8065a.svg"
+  },
+  {
+    "revision": "9a7c5f2d199ac02efaeb9873bd9d1fad",
+    "url": "/myfrontend/static/media/main_logo_black.9a7c5f2d.svg"
+  },
+  {
+    "revision": "4ba53d47daeabf46ec82645551839c24",
+    "url": "/myfrontend/static/media/mastercard_icon.4ba53d47.svg"
+  },
+  {
+    "revision": "76c3dcd4b2e169faf82b2efb62cba414",
+    "url": "/myfrontend/static/media/mdi_delete-forever-outline.76c3dcd4.svg"
+  },
+  {
+    "revision": "3964f054ee9d404f4f4d9841d37109ed",
+    "url": "/myfrontend/static/media/mdi_deletePink.3964f054.svg"
+  },
+  {
+    "revision": "57427f7c93b5d7748cd4718d2901dd99",
+    "url": "/myfrontend/static/media/mdi_file-document-edit-outline.57427f7c.svg"
+  },
+  {
+    "revision": "3dae0d07deb06d60929e57634be636ac",
+    "url": "/myfrontend/static/media/plusBtn_icon.3dae0d07.svg"
+  },
+  {
+    "revision": "ecf69d53b6e3d2ba6edec66269c5b21e",
+    "url": "/myfrontend/static/media/product_list_head.ecf69d53.png"
+  },
+  {
+    "revision": "fdcf2ebc09a8626ca8a38e6f479f8523",
+    "url": "/myfrontend/static/media/ring_icon.fdcf2ebc.svg"
+  },
+  {
+    "revision": "fd5e3388e6850fba1717627751a21a57",
+    "url": "/myfrontend/static/media/search.fd5e3388.svg"
+  },
+  {
+    "revision": "9124c2bc7555029c7800cfe8906c2f91",
+    "url": "/myfrontend/static/media/user-check.9124c2bc.svg"
+  },
+  {
+    "revision": "c0982fd98d0a60e9cdf6dd7be05b15e4",
+    "url": "/myfrontend/static/media/user_icon.c0982fd9.svg"
+  },
+  {
+    "revision": "f164c3b29a93e4cbe5d9a8574453eb75",
+    "url": "/myfrontend/static/media/visa_icon.f164c3b2.svg"
+  },
+  {
+    "revision": "e4afbe10117dd4b47a6dff84937d5171",
+    "url": "/myfrontend/static/media/vk_icon.e4afbe10.svg"
+  },
+  {
+    "revision": "2d4ebd19041d954ff5fa822424fb8a7e",
+    "url": "/myfrontend/static/media/yandex_icon.2d4ebd19.svg"
+  }
+]);
